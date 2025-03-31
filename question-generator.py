bd1a5b92b37122153a26297e8341038b4e068fa8
projectings <112644672+projectings@users.noreply.github.com>
2025-03-31 23:25:54 +0100

    Update question-generator.py

--- question-generator.py
+++ question-generator.py
@@ -1,9 +1,3 @@
-import sys
-import os
-
-# Add the project root to the path so we can import the existing modules
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
 from question_modifier import QuestionModifier
 
 class QuestionGenerator:
